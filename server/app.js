d78cedccca200b973ed2e23012b70101a70e98fc
aniket umbc <aniket.umbc@gmail.com>
2019-05-05 17:25:15 +0530

    added graphql with express

--- server/app.js
+++ server/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');  // allow express to understand graph ql
 
 const app = express();
+
+app.use('/graphql',graphqlHTTP({
+
+}));
 
 
 app.listen(4000,()=>{
