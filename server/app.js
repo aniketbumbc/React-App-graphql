258eefad03fbf0d7df9f1f47841506739a976fdb
aniket umbc <aniket.umbc@gmail.com>
2019-05-07 06:47:07 +0530

    mongodb connection added

--- server/app.js
+++ server/app.js
@@ -3,7 +3,14 @@ const graphqlHTTP = require('express-graphql'); // allow express to understand g
 
 const schema = require('./schema/schema');
 
+//const mongoose = require('mongoose');
+
 const app = express();
+
+// mongoose.connect('mongodb+srv://bunny:Aniket@gql-info-bvnef.mongodb.net/test?retryWrites=true');
+// mongoose.connection.once('open',()=>{
+//     console.log("connected to Database");
+// });
 
 app.use('/graphql', graphqlHTTP({
     schema,
